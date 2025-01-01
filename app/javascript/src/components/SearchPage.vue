76bffcf33c36f7549a1d7a23b9d10bb03afd832f
mikohatsu <allenhong36@gmail.com>
2025-01-02 03:35:22 +0900

    pug lang

--- app/javascript/src/components/SearchPage.vue
+++ app/javascript/src/components/SearchPage.vue
@@ -1,34 +1,30 @@
-<template>
-  <div class="l-search">
-    <div class="c-search">
-      <div class="c-search__logo">
-        <router-link to="/" class="c-search__home-link">Platform</router-link>
-      </div>
-      <div class="c-search__container">
-        <div class="c-search__box">
-          <i class="fas fa-search c-search__icon"></i>
-          <input 
-            type="text" 
-            class="c-search__input" 
-            v-model="searchQuery"
-            @keyup.enter="handleSearch"
-            placeholder="검색어를 입력하세요..."
-          >
-          <i 
-            v-if="searchQuery" 
-            class="fas fa-times c-search__clear"
-            @click="clearSearch"
-          ></i>
-        </div>
-        <button 
-          class="btn btn--primary"
-          @click="handleSearch"
-        >
-          검색
-        </button>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.l-search
+  .c-search
+    .c-search__logo
+      router-link.c-search__home-link(to="/") Platform
+    
+    .c-search__container
+      .c-search__box
+        i.fas.fa-search.c-search__icon
+        input.c-search__input(
+          type="text"
+          v-model="searchQuery"
+          @keyup.enter="handleSearch"
+          placeholder="검색어를 입력하세요..."
+        )
+        i.fas.fa-times.c-search__clear(
+          v-if="searchQuery"
+          @click="clearSearch"
+        )
+      
+      button.btn.btn--primary(
+        @click="handleSearch"
+      ) 검색
+    
+    .c-search__results(v-if="searchResults")
+      .c-search__query(v-if="searchResults.query")
+        | 검색어: {{ searchResults.query }}
 </template>
 
 <script>
