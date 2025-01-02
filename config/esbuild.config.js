272f2decd97d61b42404dd1099fe5bd24ea5eea3
mikohatsu <allenhong36@gmail.com>
2025-01-02 19:16:09 +0900

    esbuild

--- config/esbuild.config.js
+++ config/esbuild.config.js
@@ -4,6 +4,7 @@ const vuePlugin = require('esbuild-plugin-vue3')
 const sassPlugin = require('esbuild-sass-plugin').sassPlugin
 
 const isWatch = process.argv.includes('--watch')
+const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
   entryPoints: ['app/javascript/application.js'],
@@ -11,31 +12,38 @@ const config = {
   outdir: path.join(process.cwd(), 'app/assets/builds'),
   absWorkingDir: path.join(process.cwd()),
   publicPath: '/assets',
+  minify: isProd,
+  sourcemap: !isProd,
   plugins: [
     vuePlugin(),
-    sassPlugin()
+    sassPlugin({
+      type: 'style',
+      cssImports: true,
+      loadPaths: ['app/assets/stylesheets', 'node_modules']
+    })
   ],
   loader: { 
     '.png': 'dataurl', 
     '.svg': 'text',
     '.vue': 'js',
     '.scss': 'css',
-    '.css': 'css'
+    '.css': 'css',
+    '.sass': 'css'
   },
   define: {
-    'process.env.NODE_ENV': '"development"'
+    'process.env.NODE_ENV': isProd ? '"production"' : '"development"'
   },
   platform: 'browser',
-  format: 'iife'
+  format: 'iife',
+  assetNames: '[name]-[hash]',
+  metafile: true
 }
 
 if (isWatch) {
-  // Watch 모드
   esbuild.context(config).then(context => {
     context.watch()
     console.log('Watching for changes...')
   })
 } else {
-  // 일반 빌드
   esbuild.build(config).catch(() => process.exit(1))
 } 
